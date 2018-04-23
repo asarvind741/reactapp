5fa57153875eb9b67984543c0517cebad77321f0
asarvind741 <asarvind741@gmail.com>
2018-04-23 21:26:19 +0530

    Delete announcementDraftItemForAdmin.js

--- modules/test/src/app/announcement/directives/announcementDraftItemForAdmin.js
+++
@@ -1,6 +0,0 @@
-angular.module('admin').directive('announcementDraftItemForAdmin', function() {
-    return {
-        restrict: 'E',
-        templateUrl: "app/announcement/directives/announcementDraftItemForAdminTemplate.html"
-    }
-})
